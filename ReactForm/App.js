44144601bffe10242f1f4d2c4c34f929680494b6
Jay Patel <jaypatel32157@gmail.com>
2021-02-07 18:30:42 +0530

    Container and Component

--- ReactForm/App.js
+++ ReactForm/App.js
@@ -1,61 +1,10 @@
 import React, {Component} from "react"
+import Form from "./FormContainer"
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            firstName: "",
-            lastName: "",
-            finalFName:"",
-            finalLName:""
-            
-        }
-        this.handleChange = this.handleChange.bind(this)
-        this.handleClick = this.handleClick.bind(this)
-    }
-    
-    handleChange(event) {
-        const {name, value} = event.target
-        this.setState({
-            [name]: value
-        })
-    }
-    handleClick(event){
-        this.setState(prevState=>{
-            return{
-                finalFName:prevState.firstName,
-                finalLName:prevState.lastName,
-                firstName:"",
-                lastName: ""
-            }
-        });
-        event.preventDefault();
-    }
-    
-    render() {
-        return (
-            <form>
-                <input 
-                    type="text" 
-                    value={this.state.firstName} 
-                    name="firstName" 
-                    placeholder="First Name" 
-                    onChange={this.handleChange} 
-                />
-                <br />
-                <input 
-                    type="text" 
-                    value={this.state.lastName} 
-                    name="lastName" 
-                    placeholder="Last Name" 
-                    onChange={this.handleChange} 
-                />
-                <br/>
-                <input type="submit" name="submitbtn" value="Submit" onClick={this.handleClick}/>
-                <h1 style={{fontFamily:"sofia",color:"red"}}>{this.state.finalFName} {this.state.finalLName}</h1>
-            </form>
-        )
-    }
+function App() {
+    return (
+        <Form />
+    )
 }
 
 export default App
